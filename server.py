2ca497c52be98cb2ba18ac0ee230f074c54dfa2f
aleksandra4 <32882151+aleksandra4@users.noreply.github.com>
2017-11-21 11:05:23 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,8 @@
 #server
 # TCP Server Code
 from Crypto.Cipher import AES
-#from Crypto import Random
-#from Crypto.Util.number import getPrime
-#from Crypto.Util.number import getRandomInteger
 import hashlib
+from hashlib import blake2s
 import pickle
 import random
 import sys
@@ -12,13 +10,8 @@ import Crypto
 from ecc import getcurvebyname
 from array import array
 import socket
-from socket import *  
-#Client
-# TCP Client Code
- 
+from socket import *   
 host="127.0.0.1"            
-
-#host = "192.168.43.221"
 port=4446
 #port=12345               
 c=0
@@ -30,7 +23,6 @@ g=curve.G
 b=random.randint(1,2**255-19)
 
 s=socket(AF_INET, SOCK_STREAM)      
-#s.connect((host,port))
 s.bind((host,port))
 s.listen(1)                                           
 print ("Listening for connections.. ")
@@ -40,34 +32,17 @@ if (c==0):
 	Bob=(g.__mul__(b)) 
 else:
 	Bob=(Alice.__mul__(c)).__add__(g.__mul__(b))
-#pickle.dumps(Bob)
 q.send(pickle.dumps(Bob,pickle.HIGHEST_PROTOCOL))
 
-m=hashlib.sha256()
+m=hashlib.balke2s()
 m.update(str(Alice.__mul__(b)).encode())
 Bob_key=m.digest()
 cipher1 = AES.new(Bob_key, AES.MODE_ECB)
-en=[1]*10
-message=[1]*10
-for i in range (10):
-        en[i]=q.recv(1024)
-for i in range (10):        
-        message[i]=cipher1.decrypt(en[i])
-for i in range (10):
-        print ('Message [',i,']',message[i])
-'''
-en0=q.recv(1024)
-en1=q.recv(1024)
-message_0=cipher1.decrypt(en0)
-message_1=cipher1.decrypt(en1)
-'''
-#print ("Message from server : " + message_0.strip().decode('ascii'))
-
-#print ('Message 0: ',message_0)
-#print ('Message 0: ',message_1)
-#data=input("Enter data to be send:  ")  # Data to be send is stored in variable data from
-                                           
-#s.send(data.encode('utf-8'))   
+message=[1]*2
+for i in range (2):
+        en=q.recv(1024)       
+        message[i]=cipher1.decrypt(en)
+        print ('Message [',i,']',message[i].decode('iso-8859-15'))
  
 s.close()                            # Closes the socket 
 # End of code
