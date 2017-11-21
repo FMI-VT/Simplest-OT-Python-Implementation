2e876275d5d12ab50e5f32720d6729e617c8cd8e
spectatora <spectatora_91@abv.bg>
2017-11-21 13:28:42 +0200

    added definitions around client and server

--- client.py
+++ client.py
@@ -13,25 +13,44 @@ import socket
 import pickle
 from socket import *  
 host="127.0.0.1"                                
-port=4446                   
-               
-curve=getcurvebyname("ed25519")
-g=curve.G
+port=4446      
+
+
+
+def sendToServer():
+
+	curve=getcurvebyname("ed25519")
+	g=curve.G
+	
+	a=random.randint(1,2**255-19)                                           
+	Alice=(g.__mul__(a))
+	s.send(pickle.dumps(Alice,pickle.HIGHEST_PROTOCOL))
+	Bob=pickle.loads(s.recv(4096))
+
+
+	message= ['Jived fox nymph grabs quick waltz.', 'Glib jocks quiz nymph to vex dwarf.']
+
+	for i in range (2):
+		var=message[i].ljust(64)
+		k=blake2s()
+		k.update(str((Bob.__add__(Alice.__neg__().__mul__(i))).__mul__(a)).encode())
+		k=k.digest()
+		cipher= AES.new(k, AES.MODE_ECB)
+		en=cipher.encrypt(var.encode())
+		s.send(en)
+
+	
+
+	return;
+
 s=socket(AF_INET, SOCK_STREAM)                
 s.connect((host,port))
-a=random.randint(1,2**255-19)                                           
-Alice=(g.__mul__(a))
-s.send(pickle.dumps(Alice,pickle.HIGHEST_PROTOCOL))
-Bob=pickle.loads(s.recv(4096))
-for i in range (2):
-	var=str(i).ljust(32)
-	k=blake2s()
-	k.update(str((Bob.__add__(Alice.__neg__().__mul__(i))).__mul__(a)).encode())
-	k=k.digest()
-	cipher= AES.new(k, AES.MODE_ECB)
-	en=cipher.encrypt(var.encode())
-	s.send(en)
 
-s.close()
+
+for i in range (100):             
+	sendToServer()       
+
+s.close()       
+
 # End of code
 
--- server.py
+++ server.py
@@ -11,38 +11,62 @@ from ecc import getcurvebyname
 from array import array
 import socket
 from socket import *   
+import time
 host="127.0.0.1"            
 port=4446
 #port=12345               
-c=0
-if (len(sys.argv)>1):
-	c=int(sys.argv[1])
-             
-curve=getcurvebyname("ed25519")
-g=curve.G
-b=random.randint(1,2**255-19)
+
+
+def readFromClient():
+
+	c=random.randint(0,1)
+
+		     
+	curve=getcurvebyname("ed25519")
+	g=curve.G
+	b=random.randint(1,2**255-19)
+
+	
+	
+	Alice=pickle.loads(q.recv(4096))
+	if (c==0):
+		Bob=(g.__mul__(b)) 
+	else:
+		Bob=(Alice.__mul__(c)).__add__(g.__mul__(b))
+	q.send(pickle.dumps(Bob,pickle.HIGHEST_PROTOCOL))
+
+	m=hashlib.blake2s()
+	m.update(str(Alice.__mul__(b)).encode())
+	Bob_key=m.digest()
+	cipher1 = AES.new(Bob_key, AES.MODE_ECB)
+	message=[1]*2
+	for i in range (2):
+		en=q.recv(1024)       
+		message[i]=cipher1.decrypt(en)
+		print ('Message [',i,']',message[i].decode('iso-8859-15'))
+	 
+	 
+	print('#########################################################')           	             
+
+
+
+	return;
+
+
 
 s=socket(AF_INET, SOCK_STREAM)      
 s.bind((host,port))
 s.listen(1)                                           
 print ("Listening for connections.. ")
 q,addr=s.accept()
-Alice=pickle.loads(q.recv(4096))
-if (c==0):
-	Bob=(g.__mul__(b)) 
-else:
-	Bob=(Alice.__mul__(c)).__add__(g.__mul__(b))
-q.send(pickle.dumps(Bob,pickle.HIGHEST_PROTOCOL))
 
-m=hashlib.blake2s()
-m.update(str(Alice.__mul__(b)).encode())
-Bob_key=m.digest()
-cipher1 = AES.new(Bob_key, AES.MODE_ECB)
-message=[1]*2
-for i in range (2):
-        en=q.recv(1024)       
-        message[i]=cipher1.decrypt(en)
-        print ('Message [',i,']',message[i].decode('iso-8859-15'))
- 
-s.close()                            # Closes the socket 
+start_time = time.time()
+
+for i in range (100):
+	readFromClient()
+
+
+print("----- %s seconds ----" %(time.time() - start_time))
+s.close()   # Closes the socket 
+
 # End of code
